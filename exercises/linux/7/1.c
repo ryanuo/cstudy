111d9a8886cefa66a639d57e413d6dd3e579ce0c
ryanuo <1577791638@qq.com>
2026-06-27 15:28:05 +0800

    feat(exercises/linux/7): 重构线程同步代码并优化打印逻辑

--- exercises/linux/7/1.c
+++ exercises/linux/7/1.c
@@ -2,60 +2,59 @@
 #include <pthread.h>
 
 #define THREAD_NUM 4
+#define PRINT_COUNT 5
 
 int g_no = 0;
-pthread_cond_t cond;
+
 pthread_mutex_t mutex;
+pthread_cond_t cond;
 
-void *pthread_worker(void *arg)
+void *worker(void *arg)
 {
-    int start_no = *(int *)arg;
+    int id = *(int *)arg;
 
-    pthread_mutex_lock(&mutex);
-
-    while (g_no != start_no)
-    {
-        pthread_cond_wait(&cond, &mutex);
-    }
-
-    int a[4] = {1, 2, 3, 4};
+    int a[] = {1, 2, 3, 4};
     int size = sizeof(a) / sizeof(a[0]);
-    int current_no = g_no;
 
-    for (int i = 0; i < size + 1; i++)
+    for (int i = 0; i < PRINT_COUNT; i++)
     {
-        for (int j = 0; j < size; j++)
+        pthread_mutex_lock(&mutex);
+
+        while (g_no % size != id)
         {
-            printf("%d", a[current_no++ % size]);
+            pthread_cond_wait(&cond, &mutex);
         }
+
+        printf("%d ", a[g_no % size]);
+
+        g_no++;
+
+        pthread_cond_broadcast(&cond);
+
+        pthread_mutex_unlock(&mutex);
     }
-
-    printf("\n");
-    g_no++;
-
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&mutex);
 
     return NULL;
-};
+}
 
-int main(int argc, char **argv)
+int main()
 {
-    int nos[THREAD_NUM] = {0, 1, 2, 3};
-    pthread_t threads[THREAD_NUM];
+    pthread_t tid[THREAD_NUM];
+    int id[THREAD_NUM];
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond, NULL);
 
     for (int i = 0; i < THREAD_NUM; i++)
     {
-        pthread_create(&threads[i], NULL, pthread_worker, &nos[i]);
+        id[i] = i;
+        pthread_create(&tid[i], NULL, worker, &id[i]);
     }
 
     for (int i = 0; i < THREAD_NUM; i++)
-    {
-        pthread_join(threads[i], NULL);
-    }
+        pthread_join(tid[i], NULL);
+
+    printf("\n");
 
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&cond);
