185d5816e3352bd929c13b669820054afe23624d
ryanuo <1577791638@qq.com>
2026-06-27 21:42:26 +0800

    feat(exercises/linux/7/2.c): 实现生产者消费者的信号量分离机制

--- exercises/linux/7/2.c
+++ exercises/linux/7/2.c
@@ -15,7 +15,8 @@ enum
 int plate = 0;
 
 sem_t empty;
-sem_t full;
+sem_t s_full;
+sem_t d_full;
 
 pthread_mutex_t mutex;
 
@@ -29,7 +30,12 @@ void *thread_producer(void *arg)
         plate = fruits[i];
         printf("妈妈放入的水果为：%s\n", fruits[i] == APPLE ? "苹果" : "香蕉");
         pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+
+        if (i == 0)
+            sem_post(&s_full);
+        else
+            sem_post(&d_full);
+
         sleep(1);
     }
 
@@ -42,7 +48,11 @@ void *thread_consumer(void *arg)
 
     int my_fruit;
 
-    sem_wait(&full);
+    if (role == 1)
+        sem_wait(&s_full);
+    else
+        sem_wait(&d_full);
+
     pthread_mutex_lock(&mutex);
     my_fruit = plate;
     plate = EMPTY;
@@ -63,7 +73,8 @@ int main(int argc, char **argv)
     pthread_t threads[MAX_THREADS];
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, 1);
-    sem_init(&full, 0, 0);
+    sem_init(&s_full, 0, 0);
+    sem_init(&d_full, 0, 0);
 
     int consumers[2] = {1, 2};
 
@@ -76,7 +87,8 @@ int main(int argc, char **argv)
     pthread_join(threads[2], NULL);
 
     sem_destroy(&empty);
-    sem_destroy(&full);
+    sem_destroy(&s_full);
+    sem_destroy(&d_full);
     pthread_mutex_destroy(&mutex);
 
     return 0;
