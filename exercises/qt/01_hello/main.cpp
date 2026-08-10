a31ebba0a0811bd6d4004973854edbcc0d29c54f
ryanuo <1577791638@qq.com>
2026-08-10 10:03:10 +0800

    feat(qt): 更新hello world示例使用对话框和布局管理器
    - 添加QDialog和QVBoxLayout头文件
    - 将QLabel放入对话框中并设置居中对齐
    - 使用垂直布局管理器组织界面元素
    - 设置对话框标题为"01_hello"

--- exercises/qt/01_hello/main.cpp
+++ exercises/qt/01_hello/main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
+#include <QDialog>
 #include <QLabel>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel label(QStringLiteral("Hello Qt 6! 第一个练习"));
-    label.resize(280, 60);
-    label.show();
-    return app.exec();
+
+    QDialog dialog;
+    dialog.setWindowTitle(QStringLiteral("01_hello"));
+    dialog.resize(320, 120);
+
+    QVBoxLayout layout(&dialog);
+    QLabel label(QStringLiteral("Hello Qt 6! \u7b2c\u4e00\u4e2a\u7ec3\u4e60"), &dialog);
+    label.setAlignment(Qt::AlignCenter);
+    layout.addWidget(&label);
+
+    return dialog.exec();
 }
