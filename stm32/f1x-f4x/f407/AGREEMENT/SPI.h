3ee378560bfa72243f4517a09b54a296da35a9b4
ryanuo <1577791638@qq.com>
2026-09-21 19:57:34 +0800

    feat(SPI): 添加 W25QXX 读写和使能功能，支持读取 ID 和擦除扇区

--- stm32/f1x-f4x/f407/AGREEMENT/SPI.h
+++ stm32/f1x-f4x/f407/AGREEMENT/SPI.h
@@ -5,4 +5,6 @@
 void SPI1_init(void);
 void W25QXX_ReadID(uint8_t *id);
 void W25QXX_Read(uint8_t *buf, uint32_t addr, uint16_t len);
+void W25QXX_Erase_Sector(uint32_t addr);
+uint8_t W25QXX_ReadSR(void);
 #endif
